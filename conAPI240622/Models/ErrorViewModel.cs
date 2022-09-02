3aea20c8f0bb49529d50bd3c9936299bfce5d148
deepak4848 <111887778+deepak4848@users.noreply.github.com>
2022-09-02 17:43:56 +0530

    Add project files.

---
+++ conAPI240622/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace conAPI240622.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
